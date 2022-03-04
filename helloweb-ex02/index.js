49b49e943e1f803475903ff2c910fd0909c2db17
masangwoo <masw23@naver.com>
2022-03-04 10:39:15 +0900

    #express2

--- helloweb-ex02/index.js
+++ helloweb-ex02/index.js
@@ -2,40 +2,39 @@ const http = require('http');
 const path = require('path');
 const express = require('express');
 
-const mainRouter = require('./routes/main');
 const helloRouter = require('./routes/hello');
+const mainRouter = require('./routes/main');
 const userRouter = require('./routes/user');
-//const { nextTick } = require('process');
 
 const port = 9090;
 
-//Application SetUp
+// Application SetUp
 const application = express()
-      //1. static resources
-      .use(express.static(path.join(__dirname + "assets")))
-      //2. request body parser
-      .use(express.urlencoded({extended : true})) // application/x-www-form-urlencoded
-      .use(express.json())                        // application/json
-      //3. view engine
-      .set('views', path.join(__dirname,"views"))
-      .set('view engine','ejs')
-      //4. request router
-      .all('*', function(req,res, next){
+    // 1. static resources
+    .use(express.static(path.join(__dirname, "assets")))
+    // 2. request body parser
+    .use(express.urlencoded({extended: true}))     // application/x-www-form-urlencoded
+    .use(express.json())                           // application/json 
+    // 3. view engine
+    .set('views', path.join(__dirname, "views"))
+    .set('view engine', 'ejs')
+    // 4. request router
+    .all('*', function(req, res, next){
         res.locals.req = req;
         res.locals.res = res;
         next();
-      })
-      .use('/', mainRouter)
-      .use('/user', userRouter)
-      .use('/hello',helloRouter);
+    })
+    .use('/', mainRouter)
+    .use('/user', userRouter)
+    .use('/hello', helloRouter); 
 
-// Server Setup
+// Server SetUp
 http
     .createServer(application)
     .on('listening', function(){
-        console.log("http server runs on "+port);
+        console.log("http server runs on " + port)
     })
-    .on('error',function(error){
+    .on('error', function(error){
         console.error(error);
     })
     .listen(port)
